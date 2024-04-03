dc1a9dd79804273107162dd27f279972931d9cd1
yahir737 <142628989+yahir737@users.noreply.github.com>
2024-04-03 13:35:13 -0700

    removed unused libraries, replaced repeatative built string with constant strings, removed unused Methods, Optimized math with PEMDAS

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,23 +10,19 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     // Forward Input
     private float forwardInput;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const string vertical = "Vertical";
+    private const string horizonal = "Horizonal";
 
     // Update is called once per frame
     void Update()
     {
         //Get the forward Input
-        forwardInput = Input.GetAxis("Vertical");
+        forwardInput = Input.GetAxis(vertical);
         //Get the horizontal Input
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxis(horizontal);
 
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward *( forwardInput * speed * Time.deltaTime));
 
         //Turn the vehicle
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
