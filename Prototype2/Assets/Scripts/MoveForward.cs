60b67be0dfe2f668e1c3e01d47d4e5342cc631f6
yahir737 <142628989+yahir737@users.noreply.github.com>
2024-04-05 13:09:29 -0700

    Removed unused libraries. Removed Empty Function, serialized public porperties, optimized Math,

--- Prototype2/Assets/Scripts/MoveForward.cs
+++ Prototype2/Assets/Scripts/MoveForward.cs
@@ -1,19 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 40.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] private float speed = 40.0f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
     }
 }
